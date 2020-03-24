e0ddd5ec45d828e9736ae2a206a2ade5bec71736
Djaber <dso@affini-tech.com>
2020-03-24 13:30:11 +0100

    amélioration index.html et ajout route /search au serveur

--- server.py
+++ server.py
@@ -2,7 +2,8 @@ from http.server import SimpleHTTPRequestHandler
 from http.server import socketserver
 
 class Server(SimpleHTTPRequestHandler):
-    FOLDER_FRONTEND = "frontend"
+    FOLDER_CLIENT="client"
+    data=[]
 
     def __init__(self,request,client_adress,server):
         super().__init__(request,client_adress,server)
@@ -10,18 +11,18 @@ class Server(SimpleHTTPRequestHandler):
     def do_GET(self):
         if self.path=="/":
             file_name="index.html"
+        elif self.path.startswith("/search?txt="):
+            file_name="index.html"
+            txt=self.path[12:]
         else:
             file_name="error.html"
-        f=open(f"{Server.FOLDER_FRONTEND}/{file_name}","rb")
+        f=open(f"{Server.FOLDER_CLIENT}/{file_name}","rb")
         self.send_response(200)
         self.send_header('Content-type',"text/html")
         self.end_headers()
         self.wfile.write(f.read())
         f.close()
 
-PORT = 8000
-
-
-Handler = Server
-httpd = socketserver.TCPServer(("", PORT), Handler)
+PORT=8000
+httpd=socketserver.TCPServer(("",PORT),Server)
 httpd.serve_forever()
