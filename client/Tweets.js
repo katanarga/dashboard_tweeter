68110b192e2fc9d09b6408dc9d3ce1f55821af09
klchai <chaikelun@gmail.com>
2020-03-26 18:34:49 +0800

    add csv in query and update ajax

--- client/Tweets.js
+++ client/Tweets.js
@@ -1,19 +1,29 @@
 var Tweets = {};
 
-// TODO: appel python ?
-// package Flask ?
-Tweets.search_by = async function () {
+function callbackFunc(response) {
+    console.log(response);
+}
+// appel python 
+// TODO: Serveur Python
+// def GET(): return search_by_...
+Tweets.search_by(input) = async function () {
     $.ajax({
-        url : '/',
-        data:{
-            user_name: $('#user_name').val(),
-            text: $('#text').val(),
-            date: $('#date').val()
-        },
-        dataType: 'JSON',
+        url : '../query_data.py',
         type: 'GET',
+        dataType: 'JSON',
+        data:{
+            "user_name": $("user_name").val(),
+            "text": $("text").val(),
+            "date": $("date").val()
+        },
         success: function(data){
+            callbackFunc(data.user_name);
             $('#div_tweets').html(data.result);
-        }
+        },
+        error: function(XMLHttpRequest, textStatus, errorThrown) {
+            alert(XMLHttpRequest.status);
+            alert(XMLHttpRequest.readyState);
+            alert(textStatus);
+           }
     });
 }
