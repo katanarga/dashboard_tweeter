113a2d824952c1a0d359f0b39ed2500239dba6db
Djaber <dso@affini-tech.com>
2020-04-08 12:38:37 +0200

    Nombre de tweets

--- client/init.js
+++ client/init.js
@@ -9,13 +9,16 @@ window.addEventListener("load",function () {
         let div_tweets = document.getElementById("div_tweets");
         let resTweets = await Tweets.search(search_bar.value);
         let tts = JSON.parse(resTweets);
+        let nb_tweets=0;
         let div_content="";
         for (var p in tts){
+            nb_tweets++;
             date=tts[p].date.replace("T"," ");
             date=date.substr(0,19);
             div_content+= "<div><p><b>"+tts[p].user_name+" | "+date+"</b></p><p>"+tts[p].text+"</p></div><hr/>";
         }
-        div_tweets.innerHTML=div_content;
+        let h3="<h3>Nombre de tweets contenant la chaine "+search_bar.value+" : "+nb_tweets+"</h3>";
+        div_tweets.innerHTML=h3+div_content;
         inQuery=false;
         btn_search.disabled = "";
     });
