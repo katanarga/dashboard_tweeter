651fc8f051f564c8178c186662340ce56eb0507e
klchai <chaikelun@gmail.com>
2020-04-10 01:24:58 +0800

    try timeout(TCP? TODO: client)?

--- server.py
+++ server.py
@@ -1,13 +1,36 @@
 from http.server import SimpleHTTPRequestHandler
 from http.server import socketserver
+from socketserver import StreamRequestHandler, TCP
+import time
+import sys
+import os
+import socket
 from query_data import *
+
+class MyStreamRequestHandler(StreamRequestHandler):
+    def __init__(self, request, client_address, server):
+        print("%s create handler..." % (time.time()))
+        StreamRequestHandler.__init__(self, request, client_address, server)
+    
+    def handle(self):
+        self.request.settimeout(3)
+        try:
+            data = self.rfile.readline().strip()
+            time.sleep(5)
+            print("%s recv from client: %s %s" % (time.time(), self.client_address, data))
+        except socket.timeout as e:
+            print("%s catch an timeout exception. %s(%s)" % (time.time(), e, self.client_address))
+            self.finish()
 
 class Server(SimpleHTTPRequestHandler):
     
-    def __init__(self,request,client_adress,server):
+    def __init__(self,request,client_adress,RequestHandlerClass):
         self.df_tweets = pd.read_csv('data/tweets.csv', encoding='utf8')
         self.folder_client="client"
-        super().__init__(request,client_adress,server)
+        super().__init__(request,client_adress,RequestHandlerClass)
+    
+    def handle_timeout(self):
+        print("%s timeout..." %time.time())
 
     def do_GET(self):
         if self.path=="/":
@@ -40,7 +63,22 @@ class Server(SimpleHTTPRequestHandler):
         self.wfile.write(f.read())
         f.close()
 
-if __name__=="__main__":
+def main():
     PORT=8000
-    httpd=socketserver.TCPServer(("",PORT),Server)
-    httpd.serve_forever()
+    httpd=socketserver.TCPServer(("",PORT),MyStreamRequestHandler)
+    httpd.timeout = 2
+    print("server running...")
+    # httpd.serve_forever()
+    while True:
+        httpd.handle_request()
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("Keyboard Interrupted. Server shutdown.")
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)
